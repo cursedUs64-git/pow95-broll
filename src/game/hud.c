f01d8e09705345d7237c63ada44f38ca217c8a3b
cursedUs64-git <110907873+cursedUs64-git@users.noreply.github.com>
2023-07-25 14:41:37 +0530

    Update hud.c

--- src/game/hud.c
+++ src/game/hud.c
@@ -261,7 +261,7 @@ void render_hud_power_meter(void) {
  * Renders the amount of lives Mario has.
  */
 void render_hud_mario_lives(void) {
-    print_text(33, 210, ","); // 'Mario Head' glyph
+    print_text(32, 210, ","); // 'Mario Head' glyph
     print_text(49, 210, "*"); // 'X' glyph
     print_text_fmt_int(61, 210, "%02d", gHudDisplay.lives);
 }
